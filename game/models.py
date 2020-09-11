52267cb7a3b2bedddce120930eb767c3da3c8356
unknown <vessaldaneshvar@gmail.com>
2020-09-11 20:04:06 +0430

    game model

--- game/models.py
+++ game/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class game(models.Model):
+    name = models.CharField(max_length=40,null=False,verbose_name="اسم بازی")
+    describtions = models.TextField()
+    score = models.IntegerField()
+
